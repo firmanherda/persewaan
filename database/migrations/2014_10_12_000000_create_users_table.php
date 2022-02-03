680e6c1a38b4776592e44087c8f35f238c32706b
firmanherda <firmanherda26@gmail.com>
2022-02-03 21:15:08 +0700

    tes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,6 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
-            $table->string('username');
             $table->string('password');
             $table->string('email')->unique();
             //$table->timestamp('email_verified_at')->nullable();
